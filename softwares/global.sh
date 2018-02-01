03f9d26f27650f8b112cbce4453ec9212f626b3f
Guilherme Louro <guipclouro@gmail.com>
2018-02-01 16:33:16 -0200

    Update global.sh

--- softwares/global.sh
+++ softwares/global.sh
@@ -15,9 +15,12 @@ brew cask install google-chrome 2> /dev/null
 # dev apps
 brew cask install imageoptim 2> /dev/null
 brew cask install iterm2 2> /dev/null
-brew cask install sublime-text 2> /dev/null
 brew cask install visual-studio-code 2> /dev/null
 brew cask install sequel-pro 2> /dev/null
+
+# utils
+brew cask install zoomus 2> /dev/null
+brew cask install jumpshare 2> /dev/null
 
 # apps
 brew cask install vlc 2> /dev/null
