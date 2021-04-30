3c0c884c0077096821f2f64f5c32c4e37f74fc78
Guilherme Louro <guipclouro@gmail.com>
2021-04-30 12:09:20 -0300

    chore: change softwares to install

--- softwares/global.sh
+++ softwares/global.sh
@@ -15,24 +15,24 @@ brew install heroku/brew/heroku
 brew install github/gh/gh
 
 # Browser
-brew cask install google-chrome 2> /dev/null
+# brew cask install google-chrome 2> /dev/null
 
 # dev apps
 brew cask install imageoptim 2> /dev/null
 brew cask install iterm2 2> /dev/null
 brew cask install visual-studio-code 2> /dev/null
-brew cask install sequel-pro 2> /dev/null
+# brew cask install sequel-pro 2> /dev/null
 
 # utils
-brew cask install zoomus 2> /dev/null
+# brew cask install zoomus 2> /dev/null
 brew cask install kap 2> /dev/null
-brew cask install transmission 2> /dev/null
+# brew cask install transmission 2> /dev/null
 brew cask install handbrake 2> /dev/null
 
 # apps
 brew cask install vlc 2> /dev/null
 brew cask install spotify 2> /dev/null
-brew cask install spotmenu 2> /dev/null
+# brew cask install spotmenu 2> /dev/null
 brew cask install slack 2> /dev/null
-brew cask install coconutbattery 2> /dev/null
-brew cask install stremio 2> /dev/null
+# brew cask install coconutbattery 2> /dev/null
+# brew cask install stremio 2> /dev/null
